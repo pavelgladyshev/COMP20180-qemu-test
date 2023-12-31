7ddf456649c4ae67207a4fcb750824e315b1cb37
Pavel Gladyshev <pavel@digitalfire.cc>
2023-12-31 15:25:03 +0000

    experimenting with jazz-led

--- kernel.c
+++ kernel.c
@@ -3,12 +3,11 @@
 void kernel_main(void) {
   extern uint64_t _stack_top;
   uint64_t heap_start = (uint64_t)&_stack_top;
-  volatile char *c = (char*) 0x200000;
+  volatile char *c = (char*) 0xffff0000;
   char a;  
 
   kprint("hello world!\n");
 
-  *c = 'A';  // Write data into practice device
-  a = *c;    // Read data from practice device
-
+  *c = 0xfe;  // Write data into practice device
+  
 }
