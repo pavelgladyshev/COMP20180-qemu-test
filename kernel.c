5ff2809689d071d752d955f4b61df77e0a10392c
Pavel Gladyshev <pavel@digitalfire.cc>
2024-01-02 17:43:01 +0000

    Displays "30" on RARS-style 7-segment LED displays

--- kernel.c
+++ kernel.c
@@ -3,11 +3,13 @@
 void kernel_main(void) {
   extern uint64_t _stack_top;
   uint64_t heap_start = (uint64_t)&_stack_top;
-  volatile char *c = (char*) 0xffff0000;
+  volatile char *c = (char*) 0xffff0010;
+  volatile int  *u = (int*) 0xffff0010;
+  volatile char *c1 = (char*) 0xffff0011;
   char a;  
 
   kprint("hello world!\n");
 
-  *c = 0xfe;  // Write data into practice device
+  *u = 0x4f3f;  // Write data into practice device displays "30" on RARS-style 7-segment displays 
   
 }
